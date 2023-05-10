b56db6f155360d4c7fcca0df6b485485b70b9200
Clara <mariaclarahbm@gmail.com>
2023-05-10 11:21:38 -0300

    add

--- script/script.js
+++ script/script.js
@@ -6,6 +6,37 @@ const inputName = document.querySelector('#input-name');
 const inputAbout = document.querySelector('#input-about');
 const userName = document.querySelector('.profile__name');
 const userAbout = document.querySelector('.profile__about');
+const add = document.querySelector('.add__button')
+const likeButton = document.querySelectorAll(".like__button");
+
+const initialCards = [
+    {
+      name: "Vale de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+    },
+    {
+      name: "Montanhas Carecas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+    },
+    {
+      name: "Parque Nacional da Vanoise ",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+    }
+  ];
+
+
 
 
 function openPopup(){
@@ -31,6 +62,4 @@ function saveUserInfo(evt){
 }
 
 saveButton.addEventListener('click', saveUserInfo);
-
-
 
