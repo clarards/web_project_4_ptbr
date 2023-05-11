99108e7b0d21cac095a20bbd5a05380ed7785fb6
Clara <mariaclarahbm@gmail.com>
2023-05-11 08:41:23 -0300

    like button activated

--- script/script.js
+++ script/script.js
@@ -1,15 +1,20 @@
-const edit = document.querySelector('.edit__button');
-const userInfo = document.querySelector('.popup__container');
+//buttons
+const editButton = document.querySelector('.edit__button');
 const closeButton = document.querySelector('.input__close__button');
+const addButton = document.querySelector('.add__button');
+const likeButton = document.querySelectorAll('.like__button');
+const deleteCardButton = document.querySelector('.trash__icon');
 const saveButton = document.querySelector('.input__save__button');
+
+//popups
+const userInfo = document.querySelector('.popup__container');
 const inputName = document.querySelector('#input-name');
 const inputAbout = document.querySelector('#input-about');
 const userName = document.querySelector('.profile__name');
 const userAbout = document.querySelector('.profile__about');
-const add = document.querySelector('.add__button')
-const likeButton = document.querySelectorAll(".like__button");
 
-const initialCards = [
+//cards
+const initialCards = [ 
     {
       name: "Vale de Yosemite",
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
@@ -36,20 +41,20 @@ const initialCards = [
     }
   ];
 
-
+  function callInitialCards(){
+    
+  }
 
 
 function openPopup(){
     userInfo.classList.add('popup__container-active');
-
 }
-
 function closePopup(){
     userInfo.classList.remove('popup__container-active');
 }
-
-edit.addEventListener('click', openPopup);
+editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
 
 function saveUserInfo(evt){
     evt.preventDefault();
@@ -58,8 +63,15 @@ function saveUserInfo(evt){
     userAbout.textContent = inputAbout.value;
 
     closePopup();
-
 }
-
 saveButton.addEventListener('click', saveUserInfo);
 
+
+function LikeIcon() {
+  likeButton.forEach(function (item) {
+    item.addEventListener("click", function (evt) {
+      item.classList.toggle("like__button-active");
+    });
+  });
+}
+LikeIcon();
